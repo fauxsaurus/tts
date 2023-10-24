72e42dffb34dde2c37db3ebe8a3f8671bbf1b5b7
minifyre <31806063+minifyre@users.noreply.github.com>
2023-10-24 17:11:33 -0400

    toggle/animate-able sidebars

--- src/App.tsx
+++ src/App.tsx
@@ -65,141 +65,143 @@ const App = () => {
 	utterance().addEventListener('start', () => setIsPlaying(true))
 
 	return (
-		<form class="layout-grid" onSubmit={event => event.preventDefault()}>
-			<header class="layout-top">
-				<button class="toggle-menu" title="options">
-					<label for="toggle-menu-options">options/Logo</label>
-				</button>
-				<h1>
-					<abbr title="Text to Speech">TTS</abbr>
-				</h1>
-				<button class="toggle-menu" title="help">
-					<label for="toggle-menu-help">?</label>
-				</button>
-			</header>
+		<>
 			<input checked hidden id="toggle-menu-options" type="checkbox" />
-			<aside class="layout-left menu-options">
-				<fieldset>
-					<legend>Speed</legend>
-					{/* @note actual values are 10-0.1 */}
-					<Slider
-						max={10}
-						min={0.1}
-						onInput={setSpeed}
-						step={0.1}
-						unit="x"
-						value={speed()}
-					/>
-				</fieldset>
-				<fieldset>
-					<legend>Voice</legend>
-					<Dropdown
-						name="option-voice"
-						onInput={value =>
-							setSelectedVoice(voices().find(voice => voice.voiceURI === value))
-						}
-						options={Object.assign(
-							{},
-							...voices().map(voice => ({[voice.voiceURI]: voice})),
-						)}
-						value={selectedVoice()}
-					/>
-				</fieldset>
-				<fieldset>
-					<legend>Volume</legend>
-					{/* actual values are 0-1 */}
-					<Slider
-						max={100}
-						min={0}
-						onInput={setVolume}
-						step={1}
-						unit="%"
-						value={volume()}
-					/>
-				</fieldset>
-				<fieldset>
-					<legend>Loop</legend>
-					<label for="option-loop" title="loop"></label>
-					<input id="option-loop" hidden type="checkbox" />
-					<span>On/Off</span>
-				</fieldset>
-				<fieldset>
-					<legend>Language</legend>
-					<Dropdown
-						disabled
-						onInput={() => void 0}
-						options={{English: 'English'}}
-						name="language"
-						value="English"
-					/>
-				</fieldset>
-				<fieldset>
-					<legend>Pitch</legend>
-					{/* note the actual values are 0-2 */}
-					<Slider
-						max={200}
-						min={0}
-						onInput={setPitch}
-						step={1}
-						unit="%"
-						value={pitch()}
-					/>
-				</fieldset>
-			</aside>
-			<textarea
-				class="layout-center"
-				onChange={event => setText(event.currentTarget.value)}
-				placeholder={CONFIG.placholder}
-			>
-				{text()}
-			</textarea>
 			<input checked hidden id="toggle-menu-help" type="checkbox" />
-			<aside class="layout-right menu-help">
-				<h2>Help</h2>
-				<p>
-					Enter text into the field on the left and hit play at the bottom to listen to
-					it.
-				</p>
-				<h3>Can I export the audio to a file?</h3>
-				<p>unavailable due to</p>
-				<h3>I found a bug, what should I do?</h3>
-				<p>
-					Please report it <a target="_blank">here</a>!
-				</p>
-				<a target="_blank">View Source Code</a>
-			</aside>
-			<footer class="layout-bottom">
-				<button disabled title="Rewind">
-					⏪
-				</button>
-				<button
-					onClick={event => {
-						event.preventDefault()
-
-						if (isPlaying()) return void synth.pause()
-						/** @note resume is *very* important. Not only does it save the user's spot, but running the code below when synth is paused crashes the entire speech synthesis service so hard that not even the [MDN demo](https://mdn.github.io/dom-examples/web-speech-api/speak-easy-synthesis/) works until the computer is restareted */
-						if (synth.paused) return void synth.resume()
-
-						const selectedVoiceRef = selectedVoice()
-						if (!selectedVoiceRef) throw new Error('No Selected Voice!')
-
-						utterance().pitch = pitch() / 100
-						utterance().rate = speed()
-						utterance().text = text()
-						utterance().voice = selectedVoiceRef
-						utterance().volume = volume() / 100
-
-						synth.speak(utterance())
-					}}
-					title={isPlaying() ? 'Pause' : 'Play'}
+			<form class="layout-grid" onSubmit={event => event.preventDefault()}>
+				<header class="layout-top">
+					<button class="toggle-menu" title="Toggle Options Menu">
+						<label for="toggle-menu-options">options/Logo</label>
+					</button>
+					<h1>
+						<abbr title="Text to Speech">TTS</abbr>
+					</h1>
+					<button class="toggle-menu" title="Toggle Help Menu">
+						<label for="toggle-menu-help">?</label>
+					</button>
+				</header>
+				<aside class="layout-left menu-options">
+					<fieldset>
+						<legend>Speed</legend>
+						{/* @note actual values are 10-0.1 */}
+						<Slider
+							max={10}
+							min={0.1}
+							onInput={setSpeed}
+							step={0.1}
+							unit="x"
+							value={speed()}
+						/>
+					</fieldset>
+					<fieldset>
+						<legend>Voice</legend>
+						<Dropdown
+							name="option-voice"
+							onInput={value =>
+								setSelectedVoice(voices().find(voice => voice.voiceURI === value))
+							}
+							options={Object.assign(
+								{},
+								...voices().map(voice => ({[voice.voiceURI]: voice})),
+							)}
+							value={selectedVoice()}
+						/>
+					</fieldset>
+					<fieldset>
+						<legend>Volume</legend>
+						{/* actual values are 0-1 */}
+						<Slider
+							max={100}
+							min={0}
+							onInput={setVolume}
+							step={1}
+							unit="%"
+							value={volume()}
+						/>
+					</fieldset>
+					<fieldset>
+						<legend>Loop</legend>
+						<label for="option-loop" title="loop"></label>
+						<input id="option-loop" hidden type="checkbox" />
+						<span>On/Off</span>
+					</fieldset>
+					<fieldset>
+						<legend>Language</legend>
+						<Dropdown
+							disabled
+							onInput={() => void 0}
+							options={{English: 'English'}}
+							name="language"
+							value="English"
+						/>
+					</fieldset>
+					<fieldset>
+						<legend>Pitch</legend>
+						{/* note the actual values are 0-2 */}
+						<Slider
+							max={200}
+							min={0}
+							onInput={setPitch}
+							step={1}
+							unit="%"
+							value={pitch()}
+						/>
+					</fieldset>
+				</aside>
+				<textarea
+					class="layout-center"
+					onChange={event => setText(event.currentTarget.value)}
+					placeholder={CONFIG.placholder}
 				>
-					{isPlaying() ? '⏸' : '⏵'}
-				</button>
-				<button disabled title="Fast Forward">
-					⏩
-				</button>
-			</footer>
-		</form>
+					{text()}
+				</textarea>
+				<aside class="layout-right menu-help">
+					<h2>Help</h2>
+					<p>
+						Enter text into the field on the left and hit play at the bottom to listen
+						to it.
+					</p>
+					<h3>Can I export the audio to a file?</h3>
+					<p>unavailable due to</p>
+					<h3>I found a bug, what should I do?</h3>
+					<p>
+						Please report it <a target="_blank">here</a>!
+					</p>
+					<a target="_blank">View Source Code</a>
+				</aside>
+				<footer class="layout-bottom">
+					<button disabled title="Rewind">
+						⏪
+					</button>
+					<button
+						onClick={event => {
+							event.preventDefault()
+
+							if (isPlaying()) return void synth.pause()
+							/** @note resume is *very* important. Not only does it save the user's spot, but running the code below when synth is paused crashes the entire speech synthesis service so hard that not even the [MDN demo](https://mdn.github.io/dom-examples/web-speech-api/speak-easy-synthesis/) works until the computer is restareted */
+							if (synth.paused) return void synth.resume()
+
+							const selectedVoiceRef = selectedVoice()
+							if (!selectedVoiceRef) throw new Error('No Selected Voice!')
+
+							utterance().pitch = pitch() / 100
+							utterance().rate = speed()
+							utterance().text = text()
+							utterance().voice = selectedVoiceRef
+							utterance().volume = volume() / 100
+
+							synth.speak(utterance())
+						}}
+						title={isPlaying() ? 'Pause' : 'Play'}
+					>
+						{isPlaying() ? '⏸' : '⏵'}
+					</button>
+					<button disabled title="Fast Forward">
+						⏩
+					</button>
+				</footer>
+			</form>
+		</>
 	)
 }
 
