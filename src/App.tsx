dd703b5e4d3cfe39825734dac98cea1096873f40
minifyre <31806063+minifyre@users.noreply.github.com>
2023-10-30 16:23:33 -0400

    +PWA tweaks
    * Added meta tags for progressive web app functionality.
    * -`.stretch` wrapper around the play button in the footer as its `height:100%` property was somehow making the page larger than 100% of the screen height.
    * tweaked button/header/footer borders to be more consistent.
    * Slightly increased UI size to be more more mobile friendly.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import './styles/'
 import {Dropdown, Slider} from './components'
 
 /** @todo
- * SVG & PNG (for iOS... come on, Apple, get your _stuff_ together, it's been a thing for over [*three* years now](https://caniuse.com/link-icon-svg) logo
+ * vertically align header `<h1>`...
+ * improve help button icon
+ * PNG (for iOS... come on, Apple, get your _stuff_ together, it's been a thing for over [*three* years now](https://caniuse.com/link-icon-svg) logo
  * make settings work once the audio has been paused
  * holding the button down should continue to increment the value until the user lifts it up (or hits the limit)
  * button to put help text into the text area & read aloud
@@ -20,8 +22,6 @@ import {Dropdown, Slider} from './components'
  * what happens if the text changes between pausing... (need a way to clear that utterance and start anew/from a certain point)
  * tweak icon to make the lines thicker (to look better on smaller elements--e.g., the options menu toggle and chrome tab favicons)
  */
-
-/* <button onClick={() => setCount(count => count + 1)}>count is {count()}</button> */
 
 const CONFIG = {
 	exampleText: ['It is better to fail in doing good than to succeed in doing evil.'],
@@ -167,30 +167,28 @@ const App = () => {
 				</aside>
 				<footer class="layout-bottom">
 					<button data-icon="rewind" disabled title="Rewind"></button>
-					<div class="stretch">
-						<button
-							data-icon={isPlaying() ? 'pause' : 'play'}
-							onClick={event => {
-								event.preventDefault()
+					<button
+						data-icon={isPlaying() ? 'pause' : 'play'}
+						onClick={event => {
+							event.preventDefault()
 
-								if (isPlaying()) return void synth.pause()
-								/** @note resume is *very* important. Not only does it save the user's spot, but running the code below when synth is paused crashes the entire speech synthesis service so hard that not even the [MDN demo](https://mdn.github.io/dom-examples/web-speech-api/speak-easy-synthesis/) works until the computer is restareted */
-								if (synth.paused) return void synth.resume()
+							if (isPlaying()) return void synth.pause()
+							/** @note resume is *very* important. Not only does it save the user's spot, but running the code below when synth is paused crashes the entire speech synthesis service so hard that not even the [MDN demo](https://mdn.github.io/dom-examples/web-speech-api/speak-easy-synthesis/) works until the computer is restareted */
+							if (synth.paused) return void synth.resume()
 
-								const selectedVoiceRef = selectedVoice()
-								if (!selectedVoiceRef) throw new Error('No Selected Voice!')
+							const selectedVoiceRef = selectedVoice()
+							if (!selectedVoiceRef) throw new Error('No Selected Voice!')
 
-								utterance().pitch = pitch() / 100
-								utterance().rate = speed()
-								utterance().text = text()
-								utterance().voice = selectedVoiceRef
-								utterance().volume = volume() / 100
+							utterance().pitch = pitch() / 100
+							utterance().rate = speed()
+							utterance().text = text()
+							utterance().voice = selectedVoiceRef
+							utterance().volume = volume() / 100
 
-								synth.speak(utterance())
-							}}
-							title={isPlaying() ? 'Pause' : 'Play'}
-						/>
-					</div>
+							synth.speak(utterance())
+						}}
+						title={isPlaying() ? 'Pause' : 'Play'}
+					/>
 					<button data-icon="fast-forward" disabled title="Fast Forward"></button>
 				</footer>
 			</form>
